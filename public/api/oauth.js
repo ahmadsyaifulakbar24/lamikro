58b2f8e84d92b5dad51711f44ece999be09f05b4
Hilmi <nurhilmi.mail@gmail.com>
2021-06-23 17:26:58 +0700

    check login redirect

--- public/api/oauth.js
+++ public/api/oauth.js
@@ -1,62 +1,61 @@
-$('#form').submit(function(e){
-	e.preventDefault()
-	$('#submit').attr('disabled',true)
-	$('#text').hide()
-	$('#loading').show()
-	$('#username').removeClass('is-invalid')
-	$('#feedback').hide()
+$('#form').submit(function(e) {
+    e.preventDefault()
+    $('#submit').attr('disabled', true)
+    $('#text').hide()
+    $('#loading').show()
+    $('#username').removeClass('is-invalid')
+    $('#feedback').hide()
 
-	let username = $('#username').val()
-	let password = $('#password').val()
-	
-	$.ajax({
-		url: api_url+'oauth',
-		type: 'POST',
-		dataType: 'JSON',
-		data: {
-			username: username,
-			password: password
-		},
-		success: function(result) {
-			let status = result.status
-			let token = result.message
-			if(status == true) {
-				$.ajax({
-					url: root+'session/oauth',
-					type: 'GET',
-					data: {
-						token: token
-					},
-					success: function(result) {
-						localStorage.setItem('token',token)
-						$.ajax({
-							url: api_url+'metadata/userdata',
-							type: 'GET',
-							dataType: 'JSON',
-							headers: {
-								'token-id': token
-							},
-							success: function(result) {
-								console.log(result)
-								console.log(root+'app/dashboard')
-								// if(result.ref_group_user == "J1") {
-								// 	$(location).attr('href',root+'app/admin')
-								// } else {
-								// 	$(location).attr('href',root+'app/dashboard')
-								// }
-							}
-						})
-					}
-				})
-			} else {
-				$('#submit').attr('disabled',false)
-				$('#password').val('')
-				$('#username').focus()
-				$('#username').addClass('is-invalid')
-				$('#feedback').show()
-				$('#text').show()
-				$('#loading').hide()
-			}
-		}
-	})
+    let username = $('#username').val()
+    let password = $('#password').val()
+
+    $.ajax({
+        url: api_url + 'oauth',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+            username: username,
+            password: password
+        },
+        success: function(result) {
+            let status = result.status
+            let token = result.message
+            if (status == true) {
+                $.ajax({
+                    url: root + 'session/oauth',
+                    type: 'GET',
+                    data: {
+                        token: token
+                    },
+                    success: function(result) {
+                        localStorage.setItem('token', token)
+                        $.ajax({
+                            url: api_url + 'metadata/userdata',
+                            type: 'GET',
+                            dataType: 'JSON',
+                            headers: {
+                                'token-id': token
+                            },
+                            success: function(result) {
+                                // console.log(result)
+                                if (result.ref_group_user == "J1") {
+                                    location.href = root + 'app/admin'
+                                } else {
+                                    location.href = root + 'app/dashboard'
+                                }
+                            }
+                        })
+                    }
+                })
+            } else {
+                $('#submit').attr('disabled', false)
+                $('#password').val('')
+                $('#username').focus()
+                $('#username').addClass('is-invalid')
+                $('#feedback').show()
+                $('#text').show()
+                $('#loading').hide()
+            }
+        }
+    })
 })
