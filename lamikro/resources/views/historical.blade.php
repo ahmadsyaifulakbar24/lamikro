d9604dc3d83786894a82a947d121ac6c4d596f67
Hilmi <nurhilmi.mail@gmail.com>
2021-07-29 11:06:06 +0700

    update historical

--- lamikro/resources/views/historical.blade.php
+++ lamikro/resources/views/historical.blade.php
@@ -31,6 +31,34 @@
                         <div class="card">
                             <div class="card-body">
                                 <!-- <div class="float-right text-muted">Mon, Jan 9th 2019 7:00 AM</div> -->
+                                <h6 class="pb-3">Versi 3.1 (2021)</h6>
+                                <ol class="pl-4">
+                                    <li>Pelengkapan data profil dan profil usaha pengguna.</li>
+                                    <li>Akses dibuat lebih cepat.</li>
+                                    <li>Penambahan akun statistik dan profil admin untuk Kementerian Koperasi dan Usaha Kecil dan Menengah Republik Indonesia.</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-auto text-center flex-column d-sm-flex">
+                        <div class="row h-50">
+                            <div class="col border-right">&nbsp;</div>
+                            <div class="col">&nbsp;</div>
+                        </div>
+                        <h5 class="m-2">
+                            <span class="px-3 py-1 bg-light" style="border-radius:100px"></span>
+                        </h5>
+                        <div class="row h-50">
+                            <div class="col border-right">&nbsp;</div>
+                            <div class="col">&nbsp;</div>
+                        </div>
+                    </div>
+                    <div class="col col-xl-10 py-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <!-- <div class="float-right text-muted">Mon, Jan 9th 2019 7:00 AM</div> -->
                                 <h6 class="pb-3">Versi 3.0 (2020)</h6>
                                 <ol class="pl-4">
                                     <li>Pembuatan tampilan baru Aplikasi Lamikro yang lebih fresh dan ciamik.</li>
