1dcd9d77b6b12e1c89ef4ccb473e661911d239d2
Hilmi <nurhilmi.mail@gmail.com>
2021-07-30 09:47:38 +0700

    git ignore footer

--- lamikro/resources/views/app/partials/footer.blade.php
+++ lamikro/resources/views/app/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <script src="{{asset('assets/js/template.js')}}"></script>
 <script>
 	const root = '{{Request::root()}}/'
-	const root_api = 'https://lamikro.com/'
+	const root_api = 'https://lekarlwig.com/lamikro'
 	const api_url = root_api + 'e_gl/api/Services/'
 	const api_references = root_api + 'e_gl/api/References/'
 	const api_chart = root_api + 'e_gl/api/_dash/'
